6c68f569564ecb9e96709e38319fbf5deb3aaa1a
CristhianGT64 <crislopez_84@hotmail.es>
2024-05-14 11:14:20 -0600

    Creacion de backend

---
+++ src/main/java/hn/perfiles/futbol/perfilfutbol/PerfilfutbolApplication.java
@@ -0,0 +1,13 @@
+package hn.perfiles.futbol.perfilfutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilfutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilfutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/perfiles/futbol/perfilfutbol/PerfilfutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.perfiles.futbol.perfilfutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilfutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
